e1b0b51e1bb83d2bff7b8f8f1e4b0677030d70a3
parth <parth.z@gurukul.org>
2024-01-12 10:10:53 +0530

    add

---
+++ StudInfo/StudInfo/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using StudInfo.Models;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace StudInfo.Controllers
+{
+    [Route("StudentInfo")]
+    public class StudentController : Controller
+    {
+        public Context _Context { get; set; }
+        public StudentController(Context context)
+        {
+            _Context = context;
+        }
+        [HttpGet]
+        public List<Student> Get()
+        {  
+            var getdata = _Context.Students.ToList();
+            return getdata;
+        }
+
+        [HttpPost]
+        public Student insertdata([FromBody] Student student)
+        {
+            var adddata = _Context.Students.Add(student);
+            _Context.SaveChanges();
+            return student;
+        }
+    }
+}
---
+++ StudInfo/StudInfo/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudInfo.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
